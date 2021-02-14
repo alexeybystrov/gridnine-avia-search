1102618f33cb41d05c880e7f28b7803b75eae046
Alexey Bystrov <alexeybystrov@me.com>
2021-02-14 19:32:42 +0300

    minor fixes

--- src/init.jsx
+++ src/init.jsx
@@ -64,9 +64,10 @@ export default (initialState) => {
     </React.StrictMode>,
     document.getElementById('root'),
   );
+
+  serviceWorker.register();
 };
 
-serviceWorker.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
